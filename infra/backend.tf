8fb8eb95b3ebe91adf5422d0b8df4edbc52178c6
Sebastian Carreira <sebastian.m.carreira@gmail.com>
2025-07-16 22:13:10 +0200

    Backend en S3

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "sa-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "sebastianmcarreira-devops"
+    key    = "terraform/states/ejercicio.tfstate"
+    region = "sa-east-1"
+  }
+}
